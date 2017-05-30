94278b0d8fd99013bf027f52aa80252d62ab654a
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-30 11:02:10 -0400

    Transfered main menu content from app component

--- SuvienRN/src/Components/MainMenu.js
+++ SuvienRN/src/Components/MainMenu.js
@@ -1,3 +1,30 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { Button, CardSection } from './common';
+
+class MainMenu extends Component {
+    render() {
+        return (
+            <Image 
+            source={require('./Images/suviensplash.png')}
+            style={styles.imageStyle}
+            >
+                <CardSection>
+                    <Button>
+                     Get Started
+                    </Button>
+                </CardSection>
+            </Image>
+        );
+    }
+}
+
+const styles = {
+    imageStyle: {
+        flex: 1,
+        height: null,
+        width: null
+    }
+};
+
+export default MainMenu;
