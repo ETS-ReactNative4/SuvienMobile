bc91c264f9634491c246680ccb10c1b68593b55e
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-31 09:17:39 -0400

    Created placeholder page for adding photos

---
+++ SuvienRN/src/Components/AddPhoto.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class AddPhoto extends Component {
+    render() {
+        return (
+            <Text>Add Photos Here!</Text>
+        );
+    }
+}
+
+export default AddPhoto;
