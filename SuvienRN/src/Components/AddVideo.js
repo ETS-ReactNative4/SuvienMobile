99b8f594ac2ebe92f6a6b2c6b4961c1cc0552404
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-31 15:30:41 -0400

    Added video player

--- SuvienRN/src/Components/AddVideo.js
+++ SuvienRN/src/Components/AddVideo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { CardSection, Button } from './common';
+import Video from 'react-native-video';
 
 class AddVideo extends Component {
-    state = { videouri: null }
+    state = { videouri: null, pauseing: true }
     onTakeVideoPress() {
         Actions.VideoPageTest();
     }
@@ -29,7 +30,14 @@ class AddVideo extends Component {
 
     }
 
+    playVideo() {
+        return (
+            this.setState({ pauseing: false })
+        );
+    }
+
     render() {
+        console.log(this.state.pauseing)
         return (
             <View style={{ marginTop: 60 }}>
                 <CardSection>
@@ -47,10 +55,39 @@ class AddVideo extends Component {
                         Add from web using Youtube
                     </Button>
                 </CardSection>
+                <TouchableOpacity style={{ height: 400, width: 400 }} onPress={this.playVideo.bind(this)}>
+                    <Video
+                    source={require('../Images/suvienvideofinal.mp4')}
+                    ref={(ref) => {
+                        this.player = ref;
+                        }}                                   // Store reference
+                    rate={1.0}                              // 0 is paused, 1 is normal.
+                    volume={1.0}                            // 0 is muted, 1 is normal.
+                    muted={false}                           // Mutes the audio entirely.
+                    paused={true}
+                    style={styles.backgroundVideo} 
+                    resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+                    repeat                           // Repeat forever.
+                    playInBackground={false}
+                    />
+                </TouchableOpacity>
             </View>
         );
     }
 }
 
+const styles = {
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    flex: 1,
+    alignSelf: 'center',
+    height: 400,
+    width: 400
+  },
+};
 
 export default AddVideo;
