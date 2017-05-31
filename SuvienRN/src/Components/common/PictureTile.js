12bbb4bae9a202db45761378c1d976a31098a116
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-31 10:40:17 -0400

    Set up the conditions underwhich to render the image then render image using camera roll uri

--- SuvienRN/src/Components/common/PictureTile.js
+++ SuvienRN/src/Components/common/PictureTile.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { CardSection } from './CardSection';
 import ImagePicker from 'react-native-image-picker';
 
 class PictureTile extends Component { 
+    state = { imageuri: null }
     onTilePress() {
         return (
             ImagePicker.launchImageLibrary(options, (response) => {
-                console.log('Response = ', response);
+                let source = { uri: response.uri };
+                console.log(source);
+                this.setState({ imageuri: source });
             }));
+    }
+    renderPhoto() {
+        if (this.state.imageuri === null) {
+            return (
+            <Text>Click me for pictures!</Text>
+            );
+        }
+        if (this.state.imageuri !== null) {
+            return (
+                <Image source={this.state.imageuri} style={{ height: 200, width: 200 }} />
+            );
+        }
     }
     render() {
         return (
             <TouchableOpacity onPress={this.onTilePress.bind(this)}>
                 <CardSection style={{ height: 400 }}>
-                    <Text>Click me for pictures!</Text>
+                    {this.renderPhoto()}
                 </CardSection>
             </TouchableOpacity>
         );
