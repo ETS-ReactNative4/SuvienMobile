7909274f2e1e0d8033c472694731f2eb9c009ccd
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-06-02 16:09:34 -0400

    Succesfully implemented a fetch then play from music library (android)

---
+++ SuvienRN/src/Components/AddAudio.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import MusicPlayerController from 'react-native-musicplayercontroller';
+import { View } from 'react-native';
+import { CardSection, Button } from './common';
+
+class AddAudio extends Component {
+    state = { audiopath: null }
+    onRecordAudioPress() {
+        MusicPlayerController.playMusic(()=>{
+            console.log('I playin!');
+    // Successfully playing
+    }, ()=>{
+    // Failed to play
+    })
+    }
+
+    onChooseMusicPress() {
+    MusicPlayerController.presentPicker(false, (metadata) => {
+        console.log(metadata[0].uri);
+        this.setState({ audiopath: metadata[0].uri });
+        console.log(this.state.audiopath);
+    }, () => {
+        console.log('Cancel');
+    });
+    }
+
+    onAddWebAudioPress() {
+        //To add later: an actual link to async storage through state
+        //const title = this.state.audiopath.toString();
+        MusicPlayerController.preloadMusic(this.state.audiopath, (metadata) => {
+            console.log('I found the music! Its:');
+            console.log(metadata);
+        }, () => {
+            console.log('I didnt find it :(');
+        });
+    }
+    
+    render() {
+        return (
+            <View style={{ marginTop: 60 }}>
+                <CardSection>
+                    <Button onPress={this.onRecordAudioPress.bind(this)}>
+                        Record Audio
+                    </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onChooseMusicPress.bind(this)}>
+                        Choose from Music Library
+                    </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onAddWebAudioPress.bind(this)}>
+                        Add from Youtube using URL
+                    </Button>
+                </CardSection>
+            </View>
+        );
+    }
+}
+
+export default AddAudio;
