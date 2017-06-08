d7a7fee9bd825de2cfa8f653628e54fc56c51501
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-06-08 12:09:11 -0400

    Installed musicplayer

--- SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
+++ SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.suvienrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNReactNativeMusicplayercontrollerPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNReactNativeMusicplayercontrollerPackage(),
             new OrientationPackage(),
             new ImagePickerPackage()
       );
