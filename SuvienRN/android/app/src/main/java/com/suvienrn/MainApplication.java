7d3f2cc3d2ce27afddc4e4e338c62cf860b887d7
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-06-08 13:35:11 -0400

    Revert "Installed musicplayer"
    This reverts commit d7a7fee9bd825de2cfa8f653628e54fc56c51501.

--- SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
+++ SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
@@ -3,7 +3,6 @@ package com.suvienrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNReactNativeMusicplayercontrollerPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNReactNativeMusicplayercontrollerPackage(),
             new OrientationPackage(),
             new ImagePickerPackage()
       );
