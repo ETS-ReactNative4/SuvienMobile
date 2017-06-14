c18fe6a86ae96b5a8854751080fcad2161e0b5de
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-06-14 15:54:31 -0400

    Added clock functionality

--- SuvienRN/src/Components/Home.js
+++ SuvienRN/src/Components/Home.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { View, AsyncStorage, Text, Dimensions, Image, ScrollView } from 'react-native';
+import { View, AsyncStorage, Text, Dimensions, Image, ScrollView, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { Header, Card, PictureTile } from './common';
 
 class Home extends Component {
-    state = { currentDate: null, greeting: null, name: null, width: null, dim: null }
+    state = { currentDate: null, greeting: null, name: null, width: null, dim: null, sizes: null, hour: null, minute: null, aorp: null, section: null }
     componentWillMount() {
         this.setState({ width: Dimensions.get('window').width });
         this.getInfo();
     }
 
     componentDidMount() {
+        this.clockUpdate();
         this.doMath();
     }
+
     async getInfo() {
         this.setState({ name: await AsyncStorage.getItem('name') });
         const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -23,19 +26,49 @@ class Home extends Component {
         const currentHour = d.getHours();
         const month = d.getMonth();
         if (currentHour < 12) {
-            this.setState({ greeting: 'It\'s a lovely morning,' });
+            this.setState({ greeting: 'It\'s a lovely morning,', section: require('../Images/morning.png') });
         }
         if (currentHour >= 12 && currentHour < 17) {
-            this.setState({ greeting: 'It\'s a lovely afternoon,' });
+            this.setState({ greeting: 'It\'s a lovely afternoon,', section: require('../Images/afternoon.png') });
         }
         if (currentHour >= 18 && currentHour < 21) {
-            this.setState({ greeting: 'It\'s a lovely evening,' });
+            this.setState({ greeting: 'It\'s a lovely evening,', section: require('../Images/evening.png') });
         }
         if (currentHour >= 22) {
-            this.setState({ greeting: 'It\'s a lovely night,' });
+            this.setState({ greeting: 'It\'s a lovely night,', section: require('../Images/night.png') });
         }
         this.setState({ currentDate: `${weekday[numbDay]}, ${months[month]} ${numbDate}, ${numbYear}` });
     }
+    
+    parseHour(i) {
+        const times = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '8', '9', '10', '11'];
+        return times[i];
+    }
+    addZero(p) {
+        if (p < 10) {
+            p = `0${p}`;
+        }
+        return p;
+    }
+    clockUpdate() {
+        setInterval(() => {
+            const dd = new Date();
+            this.setState({ hour: this.parseHour(dd.getHours()), minute: this.addZero(dd.getMinutes()) });
+            if (dd.getHours() < 12) {
+            this.setState({ greeting: 'It\'s a lovely morning,', aorp: 'am', section: require('../Images/morning.png') });
+            }
+            if (dd.getHours() >= 12 && dd.getHours() < 17) {
+            this.setState({ greeting: 'It\'s a lovely afternoon,', aorp: 'pm', section: require('../Images/afternoon.png') });
+            }
+            if (dd.getHours() >= 18 && dd.getHours() < 21) {
+            this.setState({ greeting: 'It\'s a lovely evening,', section: require('../Images/evening.png') });
+            }
+            if (dd.getHours() >= 22) {
+            this.setState({ greeting: 'It\'s a lovely night,', section: require('../Images/night.png') });
+            }
+            }, 1000);
+    }
+
     doMath() {
         const ourWidth = this.state.width;
         const pictureDim = Math.trunc(((ourWidth - 55) / 4));
@@ -57,12 +90,28 @@ class Home extends Component {
         }
 
     render() {
-        const { currentDate, greeting, name } = this.state;
-        return (
+        const { currentDate, greeting, name, width, sizes } = this.state;
+        if (this.state.sizes !== null) {
+            const finalsize = Math.trunc((width - sizes) / 2);
+            //console.log(finalsize);
+            return (
             <View>
                 <Header>
-                    <Text style={{ fontSize: 27, fontFamily: 'ClementePDag-Book' }}>{greeting} { name }!</Text>
-                    <Text style={{ fontSize: 25, fontFamily: 'ClementePDag-Book' }}>It is { currentDate }</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flex: finalsize, alignItems: 'center', justifyContent: 'flex-start', marginLeft: 60, flexDirection: 'row' }}>
+                            <Image source={this.state.section} style={{ height: 80, width: 80 }} />
+                            <Text style={{ fontSize: 30, fontFamily: 'ClementePDag-Book', marginLeft: 10 }}>{this.state.hour}:{this.state.minute} {this.state.aorp}</Text>
+                        </View>
+                        <View style={{ justifyContent: 'center', alignItems: 'center', flex: finalsize }} onLayout={(event) => { this.setState({ sizes: event.nativeEvent.layout.width });}}>
+                            <Text style={{ fontSize: 27, fontFamily: 'ClementePDag-Book' }}>{greeting} { name }!</Text>
+                            <Text style={{ fontSize: 25, fontFamily: 'ClementePDag-Book' }}>It is { currentDate }</Text>
+                        </View>
+                        <View style={{ alignItems: 'flex-end', flex: finalsize }}>
+                            <TouchableWithoutFeedback onPress={() => Actions.Settings()}>
+                                <Image source={require('../Images/settings.png')} style={{ height: 80, width: 80 }} />
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
                 </Header>
                 <ScrollView>
                     <View style={{ marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap' }}>
@@ -71,6 +120,30 @@ class Home extends Component {
                 </ScrollView>
             </View>
         );
+    } 
+        return (
+            <View>
+                <Header>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }} onLayout={(event) => { this.setState({ sizes: event.nativeEvent.layout.width });}}>
+                            <Text style={{ fontSize: 27, fontFamily: 'ClementePDag-Book' }}>{greeting} { name }!</Text>
+                            <Text style={{ fontSize: 25, fontFamily: 'ClementePDag-Book' }}>It is { currentDate }</Text>
+                        </View>
+                        <View style={{ alignItems: 'flex-end' }}>
+                            <TouchableWithoutFeedback onPress={() => Actions.Settings()}>
+                                <Image source={require('../Images/settings.png')} style={{ height: 80, width: 80 }} />
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
+                </Header>
+                <ScrollView>
+                    <View style={{ marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap' }}>
+                        {this.renderTiles()}
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    
     }
 }
 
