fb6da09fb8d0f61360e3319db455dc82ea6607d4
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-06-14 10:39:15 -0400

    Cleared picture tile and created home page layout

--- SuvienRN/src/Components/Home.js
+++ SuvienRN/src/Components/Home.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
+import { View, AsyncStorage, Text, Dimensions, Image, ScrollView } from 'react-native';
 import { Header, Card, PictureTile } from './common';
 
 class Home extends Component {
+    state = { currentDate: null, greeting: null, name: null, width: null, dim: null }
+    componentWillMount() {
+        this.setState({ width: Dimensions.get('window').width });
+        this.getInfo();
+    }
+
+    componentDidMount() {
+        this.renderTiles();
+    }
+    async getInfo() {
+        this.setState({ name: await AsyncStorage.getItem('name') });
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const d = new Date();
+        const numbDay = d.getDay();
+        const numbDate = d.getDate();
+        const numbYear = d.getFullYear();
+        const currentHour = d.getHours();
+        const month = d.getMonth();
+        if (currentHour < 12) {
+            this.setState({ greeting: 'It\'s a lovely morning,' });
+        }
+        if (currentHour >= 12 && currentHour < 17) {
+            this.setState({ greeting: 'It\'s a lovely afternoon,' });
+        }
+        if (currentHour >= 18 && currentHour < 21) {
+            this.setState({ greeting: 'It\'s a lovely evening,' });
+        }
+        if (currentHour >= 22) {
+            this.setState({ greeting: 'It\'s a lovely night,' });
+        }
+        this.setState({ currentDate: `${weekday[numbDay]}, ${months[month]} ${numbDate}, ${numbYear}` });
+    }
+    renderTiles() {
+        const ourWidth = this.state.width;
+        const pictureDim = Math.trunc(((ourWidth - 55) / 4));
+        this.setState({ dim: pictureDim });
+    }
     render() {
+        const { currentDate, greeting, name } = this.state;
         return (
-            <Card>
-                <Header headerText="It's a lovely day!" />
-                <PictureTile />
-            </Card>
+            <View>
+                <Header>
+                    <Text style={{ fontSize: 27, fontFamily: 'ClementePDag-Book' }}>{greeting} { name }!</Text>
+                    <Text style={{ fontSize: 25, fontFamily: 'ClementePDag-Book' }}>It is { currentDate }</Text>
+                </Header>
+                <ScrollView>
+                    <View style={{ marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap' }}>
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                        <Image source={require('../Images/nocontent.jpg')} style={{ marginLeft: 5, height: this.state.dim, width: this.state.dim }} />
+                    </View>
+                </ScrollView>
+            </View>
         );
     }
 }
--- SuvienRN/src/Components/common/PictureTile.js
+++ SuvienRN/src/Components/common/PictureTile.js
@@ -1,56 +1,15 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, Image } from 'react-native';
 import { CardSection } from './CardSection';
-import ImagePicker from 'react-native-image-picker';
 
 class PictureTile extends Component { 
-    state = { imageuri: null }
-    onTilePress() {
-        return (
-            ImagePicker.launchImageLibrary(options, (response) => {
-                const source = { uri: response.uri };
-                if (source.uri === undefined) {
-                    source.uri = null;
-                }
-                this.setState({ imageuri: source });
-                console.log(this.state.imageuri);
-            }));
-    }
-    
-    renderPhoto() {
-        if (this.state.imageuri === null) {
-            return (
-            <Text>Click me for pictures!</Text>
-            );
-        }
-        if (this.state.imageuri !== null) {
-            return (
-                <Image source={this.state.imageuri} style={{ height: 200, width: 200 }} />
-            );
-        }
-    }
-    
+    state = { imageuri: null }   
     render() {
         return (
-            <TouchableOpacity onPress={this.onTilePress.bind(this)}>
-                <CardSection style={{ height: 400 }}>
-                    {this.renderPhoto()}
-                </CardSection>
-            </TouchableOpacity>
+            <Text>Weeee</Text>
         );
     }
     
 }
-
-const options = {
-            title: 'Select Avatar',
-            customButtons: [
-                { name: 'fb', title: 'Choose Photo from Facebook' },
-                ],
-                storageOptions: {
-                    skipBackup: true,
-                    path: 'images'
-                }
-};
 
 export { PictureTile };
