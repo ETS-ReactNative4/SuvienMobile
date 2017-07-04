3965eeaf02ea6b3e37efe31de183a25d57ac77a8
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-07-04 17:46:50 -0400

    Modified the check box component

--- SuvienRN/src/Components/common/CheckBox.js
+++ SuvienRN/src/Components/common/CheckBox.js
@@ -4,12 +4,23 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 class CheckBox extends Component {
     state = { checked: false, image: require('./unchecked.png') }
     componentWillMount() {
-        if (this.props.value !== null) {
+        if (this.props.checkType === 'Preferences') {
+            if (this.props.value !== null) {
+                if (this.props.value[this.props.label] === true) {
+                    this.setState({ checked: true, image: require('./checked.png') });
+                }
+                if (this.props.value[this.props.label] === false) {
+                    this.setState({ checked: false, image: require('./unchecked.png') });
+                }
+            }
+        } else {
+            if (this.props.value !== null) {
             console.log(this.props.value);
             const det = this.props.value.find((val) => val === this.props.label);
             if (det !== undefined) {
                 this.setState({ checked: true, image: require('./checked.png') });
             }
+        }
         }
     }
 
@@ -24,7 +35,17 @@ class CheckBox extends Component {
         }
     }
     render() {
-        return (
+        if (this.props.checkType === 'Preferences') {
+            return (
+            <View style={{ flexDirection: 'row', marginLeft: 10, marginBottom: 10 }}>
+                <TouchableOpacity onPress={this.checkBox.bind(this)}>
+                    <Image source={this.state.image} style={{ height: 30, width: 30 }} />
+                </TouchableOpacity>
+                <Text style={{ marginLeft: 10, fontSize: 20, fontFamily: 'Roboto-Light' }}>{this.props.label}</Text>
+            </View>
+    );
+        } else {
+            return (
             <View style={{ flexDirection: 'row', marginLeft: 10 }}>
                 <TouchableOpacity onPress={this.checkBox.bind(this)}>
                     <Image source={this.state.image} style={{ height: 30, width: 30 }} />
@@ -32,6 +53,7 @@ class CheckBox extends Component {
                 <Text style={{ marginLeft: 10, fontSize: 20, fontFamily: 'Roboto-Light' }}>{this.props.label}</Text>
             </View>
     );
+        }
     }
     
 }
