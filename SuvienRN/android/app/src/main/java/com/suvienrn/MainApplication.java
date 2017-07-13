b654aa0acffde25b53ad9a11f391b41e6348aafc
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-07-13 11:15:53 -0400

    Installed react-native-fs

--- SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
+++ SuvienRN/android/app/src/main/java/com/suvienrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.suvienrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.reactlibrary.RNReactNativeMusicplayercontrollerPackage;
 import com.github.yamill.orientation.OrientationPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ReactNativeYouTube(),
             new RNReactNativeMusicplayercontrollerPackage(),
             new OrientationPackage(),
